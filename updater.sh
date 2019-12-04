934502c0b692f15b144c52a099f4bca45247d246
Jimmy Delgado <Marikawn@gmail.com>
2019-12-04 02:39:33 -0600

    Added some logic, will continue on checking for valid user/group

--- updater.sh
+++ updater.sh
@@ -10,20 +10,60 @@
 ## This file will need to be created manually; initially.
 ## SERVER_FILE = Same as SERVER_VERSION, lists the latest version of the Minecraft server file.
 
-MINECRAFT_USER=jdelgado
-MINECRAFT_GROUP=jdelgado
-MINECRAFT_DIR=/home/minecraft/minecraft/
+#MINECRAFT_USER=jdelgado
+#MINECRAFT_GROUP=jdelgado
+
+if [ -z $MINECRAFT_USER ]
+then
+    read -p "Enter the username that owns the Minecraft Server directory: " $MINECRAFT_USER
+fi
+
+if [ -z $MINECRAFT_GROUP ]
+then
+    read -p "Enter the group that owns that Minecraft Server directory: " $MINECRAFT_GROUP
+fi
+
+### LEFT OFF HERE, WILL CONTINUE TOMOROW
+if [ -z $(grep $MINECRAFT_USER /etc/passwd) ] || [ -z $(grep $MINECRAFT_GROUP /etc/group) ]
+then
+    echo You have entered an invalid user or group!
+    exit 1
+fi
+
+MINECRAFT_DIR=/home/minecraft/minecraft
 DESKTOP_FILE=/usr/share/applications/minecraft-server.desktop
-VERSION_FILE=$MINECRAFT_DIR/version.txt
+VERSION_FILE=$MINECRAFT_DIR/version2.txt
 CURL_OUT=$(curl -ks https://www.minecraft.net/en-us/download/server/ | egrep -i "href" | egrep -i "server.jar")
 DOWNLOAD_URL=$(echo $CURL_OUT | sed 's/.*\"\(.*\)\".*/\1/' | tr -d '\r')
 SERVER_FILE=$(echo $CURL_OUT | sed 's/.*>\(.*\)<\/a>/\1/'i | tr -d '\r')
 SERVER_VERSION=$SERVER_FILE
 
+if [ ! -e $MINECRAFT_DIR ]
+then
+    echo The directory $MINECRAFT_DIR does not exist!
+    exit 1
+fi
+
 if [ ! -e $VERSION_FILE ]
 then
     echo -e The file $VERSION_FILE does not exist!
-    exit 1
+    POSSIBLE_SERVER_NAME=$(ls $MINECRAFT_DIR/minecraft_server* | sort | tail -n 1 | xargs -0 basename)
+    if [ -n $POSSIBLE_SERVER_NAME ]
+    then
+        read -p "I think your latest file is $POSSIBLE_SERVER_NAME, do you want me to create the version file for you? Enter 'y' for yes: " RESPONSE
+        if [ $RESPONSE = "y" ] || [ $RESPONSE = "Y" ]
+        then
+            echo Creating $VERSION_FILE ...
+            echo $POSSIBLE_SERVER_NAME >> $VERSION_FILE
+            chown $MINECRAFT_USER:$MINECRAFT_GROUP $VERSION_FILE
+            chmod 664 $VERSION_FILE
+        else
+            echo Ok then, create your own version file and re-run the script.
+            exit 1
+        fi 
+    else
+        exit 1
+    fi
 fi
 
 if [ ! -r $VERSION_FILE ] 
