dd1502871b6751db231ef0ca5fb222f365027c6c
Jimmy Delgado <Marikawn@gmail.com>
2019-12-13 21:39:32 -0600

    Adding ignore file, continuing to improve script

--- updater.sh
+++ updater.sh
@@ -10,8 +10,8 @@
 ## This file will need to be created manually; initially.
 ## SERVER_FILE = Same as SERVER_VERSION, lists the latest version of the Minecraft server file.
 
-#MINECRAFT_USER=jdelgado
-#MINECRAFT_GROUP=jdelgado
+MINECRAFT_USER=jdelgado
+MINECRAFT_GROUP=jdelgado
 
 if [ -z $MINECRAFT_USER ]
 then
@@ -23,18 +23,18 @@ then
     read -p "Enter the group that owns that Minecraft Server directory: " $MINECRAFT_GROUP
 fi
 
-### LEFT OFF HERE, WILL CONTINUE TOMOROW
-if [ -z $(grep $MINECRAFT_USER /etc/passwd) ] || [ -z $(grep $MINECRAFT_GROUP /etc/group) ]
-then
-    echo You have entered an invalid user or group!
-    exit 1
-fi
+### LEFT OFF HERE, WILL CONTINUE TOMORROW
+#if [ -z $(grep $MINECRAFT_USER /etc/passwd) ] || [ -z $(grep $MINECRAFT_GROUP /etc/group) ]
+#then
+#    echo You have entered an invalid user or group!
+#    exit 1
+#fi
 
 MINECRAFT_DIR=/home/minecraft/minecraft
 DESKTOP_FILE=/usr/share/applications/minecraft-server.desktop
-VERSION_FILE=$MINECRAFT_DIR/version2.txt
+VERSION_FILE=$MINECRAFT_DIR/version.txt
 CURL_OUT=$(curl -ks https://www.minecraft.net/en-us/download/server/ | egrep -i "href" | egrep -i "server.jar")
-DOWNLOAD_URL=$(echo $CURL_OUT | sed 's/.*\"\(.*\)\".*/\1/' | tr -d '\r')
+DOWNLOAD_URL=$(echo $CURL_OUT | sed 's/.*\"\(.*\.jar\)\".*/\1/' | tr -d '\r')
 SERVER_FILE=$(echo $CURL_OUT | sed 's/.*>\(.*\)<\/a>/\1/'i | tr -d '\r')
 SERVER_VERSION=$SERVER_FILE
 
@@ -81,12 +81,19 @@ then
    exit 0
 else
    echo Your system is out-of-date
-   curl -ks $DOWNLOAD_URL -o $SERVER_VERSION
-   chmod 664 $SERVER_FILE
-   chown $MINECRAFT_USER:$MINECRAFT_GROUP $SERVER_FILE
-   mv $SERVER_VERSION $MINECRAFT_DIR
-   sed -i "s/$CURRENT_VERSION/$SERVER_VERSION/g" $VERSION_FILE
-   sed -i "s/$CURRENT_VERSION/$SERVER_VERSION/g" $DESKTOP_FILE
-   echo Done! You now have the following version installed: $SERVER_VERSION
-   exit 0
+   echo Latest Version is $SERVER_FILE
+   curl -ks $DOWNLOAD_URL -o $MINECRAFT_DIR/$SERVER_VERSION
+   if [ -e $MINECRAFT_DIR/$SERVER_VERSION ]
+   then
+       chmod 664 $MINECRAFT_DIR/$SERVER_FILE
+       chown $MINECRAFT_USER:$MINECRAFT_GROUP $MINECRAFT_DIR/$SERVER_FILE
+       sed -i "s/$CURRENT_VERSION/$SERVER_VERSION/g" $VERSION_FILE
+       sed -i "s/$CURRENT_VERSION/$SERVER_VERSION/g" $DESKTOP_FILE
+       echo Done! You now have the following version installed: $SERVER_VERSION
+       exit 0   
+   else
+    echo $MINECRAFT_DIR/$MINECRAFT_VERSION Doen\'t exist! Something went wrong with the download.   
+    echo $DOWNLOAD_URL
+    exit 1
+   fi
 fi
